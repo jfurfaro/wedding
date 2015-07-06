fe36d8a0471277fca29e26dee814f0b457e22672
Joe Furfaro <joe.furfaro@thecontrolgroup.com>
2015-07-05 22:02:22 -0700

    Basic server

--- app/server.js
+++ app/server.js
@@ -1,1 +1,11 @@
-console.log('server');
+var express = require('express');
+var app = express();
+var r = require('rethinkdb');
+
+app.get('/', function(req, res){
+	res.status(200).send('We\'re getting married!');
+});
+
+var server = app.listen(4000, function(){
+	console.log('Running!');
+});
