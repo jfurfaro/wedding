cfd83ecd1097925b5ee2d3840bde437d5a867c61
Joe Furfaro <joe.furfaro@thecontrolgroup.com>
2015-08-03 08:51:07 -0700

    Added more content and Google Maps API

--- src/script.js
+++ src/script.js
@@ -5,15 +5,40 @@ window.$ = window.jQuery = require('jquery');
 require('waypoints/lib/jquery.waypoints.min.js');
 require('waypoints/lib/shortcuts/sticky.min.js');
 
-var stickyHeader = new Waypoint.Sticky({
-	element: $('header')[0]
-});
+$(function(){
+	var stickyHeader = new Waypoint.Sticky({
+		element: $('header')[0]
+	});
 
-// Doing this half in vanilla JS and no one can stop me
-var hero = $('#hero')[0],
-	heroBGPosition = 0;
-$(window).scroll(function(){
-	heroBGPosition = Math.floor(0.25 * window.scrollY);
+	// Doing this half in vanilla JS and no one can stop me
+	var hero = $('#hero')[0],
+		heroBGPosition = 0;
+	$(window).scroll(function(){
+		heroBGPosition = Math.floor(0.25 * window.scrollY);
 
-	hero.style.backgroundPosition = 'center '+ heroBGPosition + 'px';
+		hero.style.backgroundPosition = 'center '+ heroBGPosition + 'px';
+	});
+
+	// --------------------------------------------------
+	// MAPS
+	
+	var leoCarrilloMap = new google.maps.Map($('#details .map')[0], {zoom: 12, center: {lat: 33.126940, lng: -117.261104}}),
+		leoCarrilloWindow = new google.maps.InfoWindow(),
+		service = new google.maps.places.PlacesService(leoCarrilloMap);
+
+	service.getDetails({placeId: 'ChIJDUlhy8103IARGm0ZFFd9Twk'}, function(place, status) {
+		if (status == google.maps.places.PlacesServiceStatus.OK) {
+			var leoCarilloMarker = new google.maps.Marker({
+				map: leoCarrilloMap,
+				position: place.geometry.location
+			});
+				leoCarrilloWindow.setContent(place.name);
+				leoCarrilloWindow.open(leoCarrilloMap, leoCarilloMarker);
+		}
+	});
+	
+	// END MAPS
+	// --------------------------------------------------
+	
 });
+	
