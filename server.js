a2115f5bdbb635f1e033274b9416231847cbb48f
Joe Furfaro <joe.furfaro@thecontrolgroup.com>
2015-08-20 21:03:15 -0700

    Lots of styling and stuffs

--- server.js
+++ server.js
@@ -6,13 +6,24 @@ var r = require('rethinkdb');
 var _ = require('lodash');
 var cons = require('consolidate');
 var Handlebars = require('handlebars');
+var compression = require('compression');
 
 // Set up Handlebars for view templating, using path to ensure Express can find the views regardless of where the app gets launched from
 app.set('views', path.resolve(__dirname + '/src'));
 app.set('view engine', 'hbs');
 app.engine('hbs', cons.handlebars);
 
-// Shortcircuit static assets
+// DB configuration
+var dbConfig = {
+		db: 'wedding', 
+		host: process.env.DB_HOST || 'localhost', 
+		port: process.env.DB_PORT || 28015
+	};
+
+// GZIP
+app.use(compression());
+
+// Short circuit static assets
 app.use(express.static(__dirname + '/public'));
 
 app.use(bodyparser.urlencoded({extended: true}));
@@ -23,33 +34,29 @@ app.get('/', function(req, res){
 });
 
 app.post('/bus', function(req, res){
-	var passengers = [];
-
-	_.forEach(req.body.passengers, function(passenger){
-		passengers.push({
+	
+	var passenger = {
 			created: r.now(),
-			name: passenger
-		});
-	});
-		
+			name: req.body.passenger
+		};
 
-	r.connect({db: 'wedding'})
+	r.connect(dbConfig)
 		.then(function(conn){
-			r.table('bus').insert(passengers).run(conn)
+			r.table('bus').insert(passenger).run(conn)
 				.then(function(response){
-					res.status(201).json({message: response.inserted + ' Passenger(s) Added'});
+					res.status(201).json({success: true, message: 'Passenger Added'});
 				})
 				.error(function(err){
 					console.log(err);
-					res.status(500).json({message: 'Something broke'});
+					res.status(500).json({success: false, message: 'Something broke'});
 				});
 		})
 		.error(function(err){
 			console.log(err);
-			res.status(500).json({message: 'Something bigger broke'});
+			res.status(500).json({success: false, message: 'Something bigger broke'});
 		});
 });
 
 var server = app.listen(4000, function(){
-	console.log('Running!');
+	console.log('Wedding Time!');
 });
--- src/script.js
+++ src/script.js
@@ -7,6 +7,9 @@ require('waypoints/lib/shortcuts/sticky.min.js');
 
 $(function(){
 
+	// --------------------------------------------------
+	// HEADER STUFF
+	
 	var stickyHeader = new Waypoint.Sticky({
 		element: $('header')[0]
 	});
@@ -16,14 +19,71 @@ $(function(){
 
 		$('html, body').animate({scrollTop: $($(this).attr('href')).offset().top - $('header').outerHeight(true)}, 700);
 	});
+	
+	// END HEADER STUFF
+	// --------------------------------------------------
+	
 
-	// Doing this half in vanilla JS and no one can stop me
+	// --------------------------------------------------
+	// PARALLAX HERO
+	
+	// Doing this half in vanilla JS and no one can stop me #sorrynotsorry
 	var hero = $('#hero')[0],
 		heroBGPosition = 0;
 	$(window).scroll(function(){
 		heroBGPosition = Math.floor(0.25 * window.scrollY);
 		hero.style.backgroundPosition = 'center '+ heroBGPosition + 'px';
 	});
+	
+	// END PARALLAX HERO
+	// --------------------------------------------------
+
+
+	// --------------------------------------------------
+	// FUNKY BUS
+	
+	$('form#bus-signup').submit(function(e){
+		// Never actually submit
+		e.preventDefault();
+
+		var passenger = $('input[name="passenger"]').val(),
+			$this = $(this);
+
+		if(passenger) {
+			$this.children().prop('disabled', true);
+
+			$.post($this.attr('action'), {passenger: passenger})
+				.done(function(response){
+					if(response.success) {
+						$('input[name="passenger"]').val('');
+
+						// Add a message
+						$this.find('.error').remove();
+						$this.append('<p>' + passenger + ' is signed up for the bus!</p>');
+					} else {
+						$this.find('.error').remove();
+						$this.append('<p class="error">Something went wrong! Try again</p>');
+						console.log(response.message);
+					}
+				})
+				.fail(function(response){
+					$this.find('.error').remove();
+					$this.append('<p class="error">Something went wrong! Try again</p>');
+					console.log(response.responseJSON.message);
+				})
+				.always(function(){
+					// Enable the form again
+					$this.children().prop('disabled', '');
+				});
+		} else {
+			$this.find('.error').remove();
+			$this.append('<p class="error">You forgot to put your name!</p>');
+		}
+	});
+	
+	// END FUNKY BUS
+	// --------------------------------------------------
+	
 
 	// --------------------------------------------------
 	// WEDDING PARTY
@@ -31,50 +91,47 @@ $(function(){
 	var $bioContainer = $('#bio'),
 		$bioFader = $bioContainer.children('div');
 
-	$('.groomsman, .bridesmaid').click(function(){
-		var $this = $(this),
-			$bio = $this.children('div');
+	if($(window).width() > 600) {
+		$('.groomsman, .bridesmaid').click(function(){
+			var $this = $(this),
+				$bio = $this.children('div');
 
-		if($this.hasClass('selected')) {
-			$this.removeClass('selected');
-			$bioContainer.children('div').fadeTo(400, 0, function(){
-				$bioFader.html('');
-				$bioContainer.height(0);
-				$bioFader.fadeTo(400, 1);	
-			});
-		} else {
-			$('#wedding-party .selected').removeClass('selected');
-			$this.addClass('selected');
-			$bioFader.fadeTo(400, 0, function(){
-				$bioFader.html($bio.html());
-				$bioContainer.height($bioFader.outerHeight(true));
-				$bioFader.fadeTo(400, 1);	
-			});
-		}
-	});
+			if($this.hasClass('selected')) {
+				$this.removeClass('selected');
+				$bioContainer.children('div').fadeTo(400, 0, function(){
+					$bioFader.html('');
+					$bioContainer.height(0).addClass('closed');
+					$bioFader.fadeTo(400, 1);	
+				});
+			} else {
+				$('#wedding-party .selected').removeClass('selected');
+				$this.addClass('selected');
+				$bioFader.fadeTo(400, 0, function(){
+					$bioFader.html($bio.html());
+					$bioContainer.height($bioFader.outerHeight(true)).removeClass('closed');
+					$bioFader.fadeTo(400, 1);	
+				});
+			}
+		});
+	} else {
+		$('.groomsman, .bridesmaid').click(function(){
+			$(this).toggleClass('selected');
+		});
+	}
 	
 	// END WEDDING PARTY
 	// --------------------------------------------------
 
 	// --------------------------------------------------
-	// MAPS
+	// HASHTAG
 	
-	// var leoCarrilloMap = new google.maps.Map($('#details .map')[0], {zoom: 12, center: {lat: 33.126940, lng: -117.261104}}),
-	// 	leoCarrilloWindow = new google.maps.InfoWindow(),
-	// 	service = new google.maps.places.PlacesService(leoCarrilloMap);
-
-	// service.getDetails({placeId: 'ChIJDUlhy8103IARGm0ZFFd9Twk'}, function(place, status) {
-	// 	if (status == google.maps.places.PlacesServiceStatus.OK) {
-	// 		var leoCarilloMarker = new google.maps.Marker({
-	// 			map: leoCarrilloMap,
-	// 			position: place.geometry.location
-	// 		});
-	// 			leoCarrilloWindow.setContent(place.name);
-	// 			leoCarrilloWindow.open(leoCarrilloMap, leoCarilloMarker);
-	// 	}
-	// });
+	if($(window).width() > 600) {
+		$('.instagram-link').click(function(){
+			return false;
+		});
+	}
 	
-	// END MAPS
+	// END HASHTAG
 	// --------------------------------------------------
 	
 });
